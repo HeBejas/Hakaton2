b059aa2601a56d55bbdf125aec8bbd7d7354524b
Ivan <106988585+qwike@users.noreply.github.com>
2023-10-11 15:22:25 +0400

    v0.3beta

--- funs.php
+++ funs.php
@@ -11,7 +11,68 @@
 </head>
 <body>
     <?include 'include/header.php'?>
-    
+    <div class="container">
+        <div class="page_content">
+            <h2>Прокат</h2>
+            <div class="information_block">В деревне Мувыр можно наслаждаться природой, прогуляться по лесу до висячего моста, прокатиться на лодке, рыбачить, организовать барбекю, устроить пикник в беседке или под открытым небом и многое другое.</div>
+        <div class="prokat_catalog">
+            <div class="prokat_position">
+                <div class="prokat_block">
+                    <img src="prokat1.PNG">
+                    <div class="prokat_name">Лодка</div>
+                    <div class="prokat_desc">30 мин Вместимость одной лодки 3 взрослых 1 ребёнок</div>
+                    <div class="prokat_priсe_block"> От <span class="prokat_priсe"> 400 </span> За человека</div>
+                </div>
+            </div>
+
+            <div class="prokat_position">
+                <div class="prokat_block">
+                    <img src="prokat1.PNG">
+                    <div class="prokat_name">Лодка</div>
+                    <div class="prokat_desc">30 мин Вместимость одной лодки 3 взрослых 1 ребёнок</div>
+                    <div class="prokat_priсe_block"> От <div class="prokat_priсe"> 400 </div> За человека</div>
+                </div>
+            </div>
+
+            <div class="prokat_position">
+                <div class="prokat_block">
+                    <img src="prokat1.PNG">
+                    <div class="prokat_name">Лодка</div>
+                    <div class="prokat_desc">30 мин Вместимость одной лодки 3 взрослых 1 ребёнок</div>
+                    <div class="prokat_priсe_block"> От <div class="prokat_priсe"> 400 </div> За человека</div>
+                </div>
+            </div>
+
+            <div class="prokat_position">
+                <div class="prokat_block">
+                    <img src="prokat1.PNG">
+                    <div class="prokat_name">Лодка</div>
+                    <div class="prokat_desc">30 мин Вместимость одной лодки 3 взрослых 1 ребёнок</div>
+                    <div class="prokat_priсe_block"> От <div class="prokat_priсe"> 400 </div> За человека</div>
+                </div>
+            </div>
+
+            <div class="prokat_position">
+                <div class="prokat_block">
+                    <img src="prokat1.PNG">
+                    <div class="prokat_name">Лодка</div>
+                    <div class="prokat_desc">30 мин Вместимость одной лодки 3 взрослых 1 ребёнок</div>
+                    <div class="prokat_priсe_block"> От <div class="prokat_priсe"> 400 </div> За человека</div>
+                </div>
+            </div>
+
+            <div class="prokat_position">
+                <div class="prokat_block">
+                    <img src="prokat1.PNG">
+                    <div class="prokat_name">Лодка</div>
+                    <div class="prokat_desc">30 мин Вместимость одной лодки 3 взрослых 1 ребёнок</div>
+                    <div class="prokat_priсe_block"> От <div class="prokat_priсe"> 400 </div> За человека</div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    </div>
     <?include 'include/footer.php'?>
     <script>
         $("#funs").addClass("active_header_btn")
