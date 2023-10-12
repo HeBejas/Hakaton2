88529c4af5dfa3bb5703124ffc8ca1c94598ffda
maximkasvetofor <106871362+maximkasvetofor@users.noreply.github.com>
2023-10-12 13:20:34 +0400

    Add files via upload

--- muvyr_admin/category/wedding.php
+++ muvyr_admin/category/wedding.php
@@ -1,12 +1,12 @@
 <div id="delete_modal" class="modal_background">
-	<div class="modal" id="mod">
-		<form id="orders_delete">
+	<div class="modal" id="mwd">
+		<form id="wedding_delete">
 			<div class="modal_flex">
 				<h2>Вы уверены, что хотите удалить?</h2>
 			</div>
-			<input type="hidden" id="hmod">
+			<input type="hidden" id="hmwd">
 			<div class="modal_flex">
-				<button type="submit" class="default_button" id="odelete_btn">Да, удалить</button>
+				<button type="submit" class="default_button" id="wdelete_btn">Да, удалить</button>
 				<div class="default_button modal_close">Нет, закрыть</div>
 			</div>
 		</form>
@@ -14,7 +14,7 @@
 </div>
 <?php
 	include("../connect.php");
-	$query="SELECT `orders_house`.`id_orders`, `house`.`name_house`, `orders_house`.`name_orders`, `orders_house`.`phone_orders` FROM `orders_house` LEFT JOIN `house` ON `orders_house`.`id_house`=`house`.`id_house`";
+	$query="SELECT * FROM orders_marry";
 	$link = mysqli_connect($host, $user, $password, $db_name); 
     $result = mysqli_query($link, $query);
     if($result == false) {
@@ -29,12 +29,11 @@
         }
         else{
 	    	echo '
-			<table border="1" id="orders_table">
+			<table border="1" id="wedding_table">
 				<thead>
 					<tr>
 						<th>№</th>
-						<th>Домик</th>
-						<th>ФИО покупателя</th>
+						<th>ФИО клиента</th>
 						<th>Телефон</th>
 						<th>Действия</th>
 					</tr>
@@ -44,10 +43,9 @@
 				echo '
 				<tr>
 					<td>'.$row['id_orders'].'</td>
-					<td>'.$row['name_house'].'</td>
 					<td>'.$row['name_orders'].'</td>
 					<td>'.$row['phone_orders'].'</td>
-					<td><div class="default_button o_delete" id="od'.$row['id_orders'].'">Удалить</div></td>
+					<td><div class="default_button w_delete" id="wd'.$row['id_orders'].'">Удалить</div></td>
 				</tr>';
 			}
 			echo '</tbody>
@@ -58,19 +56,19 @@
 
 ?>
 <script>
-	tableo = new DataTable('#orders_table', {
+	tablew = new DataTable('#wedding_table', {
     language: {
         url: '//cdn.datatables.net/plug-ins/1.13.6/i18n/ru.json',
     },
 	});
-	$(".o_delete").click(function(){
+	$(".w_delete").click(function(){
 		$("#delete_modal").css("display", "flex");
 		var del_id0 = $(this).attr("id");
 		var del_id = del_id0.substring(2, del_id0.length);
-		$("#hmod").val(del_id);
+		$("#hmwd").val(del_id);
 	});
-	$("#milk_delete").submit(function(event){
+	$("#wedding_delete").submit(function(event){
 		event.preventDefault();
-		$("#mod").load("category/delete/odelete.php", {id: $("#hmod").val()});
+		$("#mwd").load("category/delete/wdelete.php", {id: $("#hmwd").val()});
 	});
 </script>
