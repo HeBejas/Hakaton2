952524cbba5ed66e66d2ded9cdd45126d62c859d
Bogdan <bogdan.aleksandrov.01@mail.ru>
2023-10-12 08:38:23 +0400

    ивентс дапт полу

---
+++ НеАдаптированные ивенты.php
@@ -0,0 +1,226 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Мероприятия</title>
+    <link rel="icon" type="image/x-icon" href="assets/images/logo.png">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/events.css">
+    <link rel="stylesheet" href="assets/slick/slick.css">
+    <link rel="stylesheet" href="assets/slick/slick-theme.css">
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/slick/slick.min.js"></script>
+</head>
+<body>
+    <?include 'include/header.php'?>
+    <div class="container">
+        <div class="page_content">
+            <h2>Мероприятия</h2>
+            <div class="information_block">В Мувыре вас ждёт множество увлекательных мероприятий, расчитанных на то, чтобы оставить у вас самые яркие и теплиые воспоминания о нашей деревне</div>
+            <div class="events_catalog">
+                <div class="event_position wow animate__animated animate__fadeIn">
+                    <div class="event_position_time_desc">
+                        <div class="event_date">11.10 - 20.10</div>
+                        <div class="event_time">С 8.00 До 19.00</div>
+                    </div>
+                    <img src="assets/images/event1.PNG">
+                    <div class="event_position_desc_main">
+                        <div class="event_name">Масленица</div>
+                        <div class="event_desc">В Мувыре празднуется масленицаю. Всех Ждем!</div>
+                    </div>
+                </div>
+                <div class="event_position wow animate__animated animate__fadeInLeft">
+                    <div class="event_position_time_desc">
+                        <div class="event_date">11.10 - 20.10</div>
+                        <div class="event_time">С 8.00 До 19.00</div>
+                    </div>
+                    <img src="assets/images/event1.PNG">
+                    <div class="event_position_desc">
+                        <div class="event_name">Масленица</div>
+                        <div class="event_desc">В Мувыре празднуется масленицаю. Всех Ждем!</div>
+                    </div>
+                </div>
+                <div class="event_position wow animate__animated animate__fadeInRight">
+                    <div class="event_position_time_desc">
+                        <div class="event_date">11.10 - 20.10</div>
+                        <div class="event_time">С 8.00 До 19.00</div>
+                    </div>
+                    <img src="assets/images/event1.PNG">
+                    <div class="event_position_desc">
+                        <div class="event_name">Масленица</div>
+                        <div class="event_desc">В Мувыре празднуется масленицаю. Всех Ждем!</div>
+                    </div>
+                </div>
+            </div>
+            <button>Еще</button>
+            <h2>Проведение свадеб</h2>
+            <div class="information_block">Где играть свадьбу? Еще сомневайтесь? Еще не определились? 
+            Конечно в Мувыре!!!
+            Там, где просто и красиво:
+            Выездная регистрация
+            Фуршет
+            Банкет
+            Домик невесты</div>
+
+            <div class="marry_slider_box wow animate__animated animate__fadeInUp">
+                <div class="marry_slider_track_box">
+                    <div class="marry_slider_1">
+                        <div>
+                            <div class="marry_img_box1">
+                                <img src="assets/images/marry1.jpg" alt="Фото со свадьбы">
+                            </div>
+                        </div>
+                        <div>
+                            <div class="marry_img_box1">
+                                <img src="assets/images/marry2.jpg" alt="Фото со свадьбы">
+                            </div>
+                        </div>
+                        <div>
+                            <div class="marry_img_box1">
+                                <img src="assets/images/marry3.jpg" alt="Фото со свадьбы">
+                            </div>
+                        </div>
+                        <div>
+                            <div class="marry_img_box1">
+                                <img src="assets/images/marry4.jpg" alt="Фото со свадьбы">
+                            </div>
+                        </div>
+                        <div>
+                            <div class="marry_img_box1">
+                                <img src="assets/images/marry5.jpg" alt="Фото со свадьбы">
+                            </div>
+                        </div>
+                        <div>
+                            <div class="marry_img_box1">
+                                <img src="assets/images/marry6.jpg" alt="Фото со свадьбы">
+                            </div>
+                        </div>
+                        <div>
+                            <div class="marry_img_box1">
+                                <img src="assets/images/marry7.jpg" alt="Фото со свадьбы">
+                            </div>
+                        </div>
+                        <div>
+                            <div class="marry_img_box1">
+                                <img src="assets/images/marry8.jpg" alt="Фото со свадьбы">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="marry_slider_track_box">
+                    <div class="marry_slider_2">
+                        <div>
+                            <div class="marry_img_box2">
+                                <img src="assets/images/marry1.jpg" alt="Фото со свадьбы">
+                            </div>
+                        </div>
+                        <div>
+                            <div class="marry_img_box2">
+                                <img src="assets/images/marry2.jpg" alt="Фото со свадьбы">
+                            </div>
+                        </div>
+                        <div>
+                            <div class="marry_img_box2">
+                                <img src="assets/images/marry3.jpg" alt="Фото со свадьбы">
+                            </div>
+                        </div>
+                        <div>
+                            <div class="marry_img_box2">
+                                <img src="assets/images/marry4.jpg" alt="Фото со свадьбы">
+                            </div>
+                        </div>
+                        <div>
+                            <div class="marry_img_box2">
+                                <img src="assets/images/marry5.jpg" alt="Фото со свадьбы">
+                            </div>
+                        </div>
+                        <div>
+                            <div class="marry_img_box2">
+                                <img src="assets/images/marry6.jpg" alt="Фото со свадьбы">
+                            </div>
+                        </div>
+                        <div>
+                            <div class="marry_img_box2">
+                                <img src="assets/images/marry7.jpg" alt="Фото со свадьбы">
+                            </div>
+                        </div>
+                        <div>
+                            <div class="marry_img_box2">
+                                <img src="assets/images/marry8.jpg" alt="Фото со свадьбы">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="information_block">
+                <p>Организация свадьбы в Мувыре, выездные регистрации в деревне:</p>
+                <p>Вы можете договорится по телефону <span class="tel">+7 (901) 865 87-55</span></p>
+            </div>
+        </div>
+    </div>
+    <?include 'include/footer.php'?>
+    <script>
+        $("#events").addClass("active_header_btn");
+        $(".marry_slider_1").slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            fade: true,
+            asNavFor: '.marry_slider_2',
+        });
+        $(".marry_slider_2").slick({
+            slidesToShow: 5,
+            slidesToScroll: 1,
+            asNavFor: '.marry_slider_1',
+            centerMode: true,
+            focusOnSelect: true,
+        });
+    </script>
+    <script src="assets/js/wow.min.js"></script>
+    <script>
+    $(document).ready(() => {
+        new WOW().init();
+    })
+    </script>
+</body>
+</html>
+
+<style>
+    @media (max-width: 1180px){
+        .event_position:nth-child(1){
+            width: 400px !important;
+            height: 300px !important;
+        }
+        .event_position:{
+            width: 400px !important;
+            height: 300px !important;
+            margin: 25px;
+        }
+        .event_position:nth-child(1) img{
+            width: 400px;
+            height: 300px;
+        }
+        .event_position_desc:nth-child(1){
+            width: 400px !important;
+        }
+        .event_position_desc_main{
+            width: 400px !important;
+        }
+        .events_catalog{
+            width: 699px !important;
+        }
+    }
+    /* @media (max-width: 1180px){
+
+        .event_position:{
+            width: 350px !important;
+            height: 300px !important;
+        }
+        .event_position:nth-child(1){
+            width: 350px !important;
+            height: 300px !important;
+        }
+    } */
+</style>
