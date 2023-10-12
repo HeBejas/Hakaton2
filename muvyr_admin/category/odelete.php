dce6e95447c1c2d31dabdf7074de841e2003625d
maximkasvetofor <106871362+maximkasvetofor@users.noreply.github.com>
2023-10-12 13:19:10 +0400

    Add files via upload

---
+++ muvyr_admin/category/odelete.php
@@ -0,0 +1,22 @@
+<?php
+    echo '<script>
+                $(".modal_close").click(function(){
+                    $("#delete_modal").css("display", "none");
+                    location.reload();
+                });
+            </script>';
+	include("../../connect.php");
+    $query = "DELETE FROM orders_house WHERE `orders_house`.`id_orders` = '$_POST[id]'";
+    $link = mysqli_connect($host, $user, $password, $db_name); 
+    $result = mysqli_query($link, $query);
+    if($result == false) {
+        die('Произошла ошибка при выполнении запроса 
+            <div class="default_button modal_close">Закрыть</div>'); 
+    }
+    else {
+        echo'
+            Операция выполнена! 
+            <div class="default_button modal_close">Закрыть</div>
+        ';
+    }
+?>
